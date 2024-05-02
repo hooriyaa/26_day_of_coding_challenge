c898c889a7f87adaac73cd298d37b2af4283dfca
hooriya <parih3080@gmail.com>
2024-05-02 22:36:29 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict";
+// // QUESTION 76:
+// function addnumbers(num1:number,num2:number) {
+//     return num1+num2
+// }
+// let result=addnumbers(3,7);
+// console.log(result)
+// // QUESTION 77:
+// function greetToUser(name:string="anonymous"){
+//     console.log(`hello ${name}`)
+// }
+// greetToUser();
+// greetToUser("hooriya");
+// QUESTION 78:
+let funExpression = function (val2) {
+    return val2 ** 2;
+};
+let res = funExpression(6);
+console.log(res);
+function funDecleration(val1) {
+    return val1 ** 2;
+}
+let answer = funDecleration(4);
+console.log(answer);
